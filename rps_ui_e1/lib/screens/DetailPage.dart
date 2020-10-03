df8deda6461a07a54120f9ecc91da9d9e85bb4d0
Paras Jain <dreams.jain1@gmail.com>
2020-10-03 13:22:29 +0530

    Episode 1

---
+++ rps_ui_e1/lib/screens/DetailPage.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:rps_ui_e1/BorderIcon.dart';
+import 'package:rps_ui_e1/utils/constants.dart';
+import 'package:rps_ui_e1/utils/custom_functions.dart';
+import 'package:rps_ui_e1/utils/widget_functions.dart';
+
+class DetailPage extends StatelessWidget {
+  final dynamic itemData;
+
+  const DetailPage({Key key,@required this.itemData}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final ThemeData themeData = Theme.of(context);
+    final double padding = 25;
+    final sidePadding = EdgeInsets.symmetric(horizontal: padding);
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: COLOR_WHITE,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Image.asset(itemData["image"]),
+                  Positioned(
+                    width: size.width,
+                    top: padding,
+                    child: Padding(
+                      padding: sidePadding,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          InkWell(
+                            onTap: (){
+                              Navigator.pop(context);
+                            },
+                            child: BorderIcon(
+                              height: 50,
+                              width: 50,
+                              child: Icon(Icons.keyboard_backspace,color: COLOR_BLACK,),
+                            ),
+                          ),
+                          BorderIcon(
+                            height: 50,
+                            width: 50,
+                            child: Icon(Icons.favorite_border,color: COLOR_BLACK,),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              addVerticalSpace(padding),
+              Padding(
+                padding: sidePadding,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("${formatCurrency(itemData["amount"])}",style: themeData.textTheme.headline1,),
+                        addVerticalSpace(5),
+                        Text("\$${itemData["address"]}",style: themeData.textTheme.subtitle2,),
+                      ],
+                    ),
+                    BorderIcon(child: Text("20 Hours ago",style: themeData.textTheme.headline5,),padding: const EdgeInsets.symmetric(vertical: 15,horizontal: 15),)
+                  ],
+                ),
+              ),
+              addVerticalSpace(padding),
+              Padding(
+                padding: sidePadding,
+                child: Text("Housing Information",style: themeData.textTheme.headline4,),
+              ),
+              addVerticalSpace(padding),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                physics: BouncingScrollPhysics(),
+                child: Row(
+                  children: [
+                      InformationTile(content: "${itemData["area"]}",name: "Square Foot",),
+                    InformationTile(content: "${itemData["bedrooms"]}",name: "Bedrooms",),
+                    InformationTile(content: "${itemData["bathrooms"]}",name: "Bathrooms",),
+                    InformationTile(content: "${itemData["garage"]}",name: "Garage",)
+                  ],
+                ),
+              ),
+              addVerticalSpace(padding),
+              Padding(
+                padding: sidePadding,
+                child: Text("Loved this design because of it's cleanliness and UX. Everything looks on point. Informative and intuitive enough for audiences of all age groups.The typeface is also aptWhat do you think about this design? \n\n Let us know in the comments below Tag designers and inspire them to create something awesome. Turn on post notifications to never miss a design feed from us"
+                            ,textAlign: TextAlign.justify,style: themeData.textTheme.bodyText2,),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class InformationTile extends StatelessWidget{
+
+  final String content;
+  final String name;
+
+  const InformationTile({Key key,@required this.content,@required this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    final Size size = MediaQuery.of(context).size;
+    final double tileSize = size.width*0.20;
+    return Container(
+      margin: const EdgeInsets.only(left: 25),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          BorderIcon(
+              width: tileSize,
+              height: tileSize,
+              child: Text(content,style: themeData.textTheme.headline3,)),
+          addVerticalSpace(15),
+          Text(name,style: themeData.textTheme.headline6,)
+        ],
+      ),
+    );
+
+  }
+
+}
